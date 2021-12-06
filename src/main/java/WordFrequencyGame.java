d31c4f3f0bf4802c2ce285ac4f16926ce68151d2
tfwong36 <tfwong36@gmail.com>
2021-12-06 16:43:04 +0800

    [WordFrequencyRefactor][Jason]-refactor: Create constant SpaceRegex for \\s+

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,22 +3,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
-import java.io.CharArrayWriter;
-
-import java.time.LocalDateTime;
 
 public class WordFrequencyGame {
+
+    public static final String SpaceRegex = "\\s+";
+
     public String getResult(String inputStr){
-
-
-        if (inputStr.split("\\s+").length==1) {
+        if (inputStr.split(SpaceRegex).length==1) {
             return inputStr + " 1";
         } else {
 
             try {
 
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
+                String[] arr = inputStr.split(SpaceRegex);
 
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
