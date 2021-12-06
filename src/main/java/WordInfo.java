5c01851bdc462fab2e7b03da474c8445be4f388b
tfwong36 <tfwong36@gmail.com>
2021-12-06 16:51:49 +0800

    [WordFrequencyRefactor][Jason]-refactor: Rename w to value, i to count in WordInfo Class

--- src/main/java/WordInfo.java
+++ src/main/java/WordInfo.java
@@ -2,9 +2,9 @@ public class WordInfo {
     private String value;
     private int count;
 
-    public WordInfo(String w, int i){
-        this.value =w;
-        this.count =i;
+    public WordInfo(String value, int count){
+        this.value = value;
+        this.count = count;
     }
 
 
